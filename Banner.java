c9ba27336f471d945f5fb8b36eeaf9ad21b90d33
doneforlifes <nithwin.shibil@gmail.com>
2026-03-08 13:54:18 +0530

    UC5 banner using inline array initilization

--- Banner.java
+++ Banner.java
@@ -1,24 +1,17 @@
 public class Banner {
     public static void main(String[] args) {
 
-        String[] banner = new String[7];
+        String[] banner = {
+            String.join(" ", " ***** ", " ", " ***** ", " ", " ****** ", " ", " ****** "),
+            String.join(" ", "*     *", " ", "*     *", " ", "*     *", " ", "*      "),
+            String.join(" ", "*     *", " ", "*     *", " ", "*     *", " ", "*      "),
+            String.join(" ", "*     *", " ", "*     *", " ", "****** ", " ", " ***** "),
+            String.join(" ", "*     *", " ", "*     *", " ", "*      ", " ", "      *"),
+            String.join(" ", "*     *", " ", "*     *", " ", "*      ", " ", "      *"),
+            String.join(" ", " ***** ", " ", " ***** ", " ", "*      ", " ", "****** ")
+        };
 
-        banner[0] = String.join(" ",
-                " ***** ", " ", " ***** ", " ", " ****** ", " ", " ****** ");
-        banner[1] = String.join(" ",
-                "*     *", " ", "*     *", " ", "*     *", " ", "*      ");
-        banner[2] = String.join(" ",
-                "*     *", " ", "*     *", " ", "*     *", " ", "*      ");
-        banner[3] = String.join(" ",
-                "*     *", " ", "*     *", " ", "****** ", " ", " ***** ");
-        banner[4] = String.join(" ",
-                "*     *", " ", "*     *", " ", "*      ", " ", "      *");
-        banner[5] = String.join(" ",
-                "*     *", " ", "*     *", " ", "*      ", " ", "      *");
-        banner[6] = String.join(" ",
-                " ***** ", " ", " ***** ", " ", "*      ", " ", "****** ");
-
-        for(String line : banner) {
+        for (String line : banner) {
             System.out.println(line);
         }
     }
